c47b2ad64c11d6411bf1bd59e76fffff5e6b2d64
yogesh1612 <yogesh1612@users.noreply.github.com>
2020-12-23 20:27:12 +0530

    dependency_2

--- dependency.R
+++ dependency.R
@@ -1,11 +1,3 @@
 if (!require(tensorflow)) {install.packages("tensorflow")};  suppressWarnings(library(tensorflow))
 if (!require(keras)) {install.packages("keras")};  suppressWarnings(library(keras))
 install_tensorflow()
-if (!require(shiny)) {install.packages("shiny")};  suppressWarnings(library(shiny))
-if (!require(miniUI)) {install.packages("miniUI")};  suppressWarnings(library(miniUI))
-if (!require(jpeg)) {install.packages("jpeg")};  suppressWarnings(library(jpeg))
-if (!require(DT)) {install.packages("DT")};  suppressWarnings(library(DT))
-if (!require(text2vec)) {install.packages("text2vec")};  suppressWarnings(library(text2vec))
-
-
-
---
+++ dependency2.R
@@ -0,0 +1,5 @@
+if (!require(shiny)) {install.packages("shiny")};  suppressWarnings(library(shiny))
+if (!require(miniUI)) {install.packages("miniUI")};  suppressWarnings(library(miniUI))
+if (!require(jpeg)) {install.packages("jpeg")};  suppressWarnings(library(jpeg))
+if (!require(DT)) {install.packages("DT")};  suppressWarnings(library(DT))
+if (!require(text2vec)) {install.packages("text2vec")};  suppressWarnings(library(text2vec))
