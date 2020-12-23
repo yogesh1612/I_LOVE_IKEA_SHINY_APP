5f9585d2407e7c7cd0b77a6235dd468675bd34f1
Yogesh Khandelwal <yogesh1612@users.noreply.github.com>
2020-12-24 00:07:32 +0530

    Update dependency.R

--- dependency.R
+++ dependency.R
@@ -1,3 +1,3 @@
 if (!require(tensorflow)) {install.packages("tensorflow")};  suppressWarnings(library(tensorflow))
 if (!require(keras)) {install.packages("keras")};  suppressWarnings(library(keras))
-install_tensorflow()
+install_keras()
